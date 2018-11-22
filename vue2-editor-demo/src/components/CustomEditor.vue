d600d4967c602d9f5d32fae13b789a2690183ad9
Jitendra Patel <jitendra@avdevs.com>
2018-11-22 11:51:58 +0530

    remove optional dependency

--- vue2-editor-demo/src/components/CustomEditor.vue
+++ vue2-editor-demo/src/components/CustomEditor.vue
@@ -8,7 +8,6 @@
 
 <script>
     import {VueEditor} from 'vue2-editor'
-    import {ImageDrop} from 'quill-image-drop-module'
     import ImageResize from '@appsflare/quill-image-resize-module'
     import {ImageUpload} from 'quill-image-upload'
 
@@ -20,13 +19,11 @@
             return {
                 content: '',
                 customModulesForEditor: [
-                    {alias: 'imageDrop', module: ImageDrop},
                     {alias: 'imageResize', module: ImageResize},
                     {alias: 'imageUpload', module: ImageUpload}
                 ],
                 editorSettings: {
                     modules: {
-                        imageDrop: true,
                         imageResize: {
                             modules: ['Resize', 'DisplaySize', 'Toolbar']
                         },
